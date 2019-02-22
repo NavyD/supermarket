69890b547d74d3757b06b9e4660fc7b7b7fa0df4
navyd <dhjnavyd@gmail.com>
2019-02-22 17:07:39 +0800

    添加regexs使用方法注释

--- supermarket-common/src/main/java/cn/navyd/app/supermarket/util/Regexs.java
+++ supermarket-common/src/main/java/cn/navyd/app/supermarket/util/Regexs.java
@@ -4,8 +4,24 @@ public class Regexs {
 
   /**
    * 匹配接口default，用于匹配eclipse自动生成空方法
-   * <pre>default ((.|\s)+?\))\s*\{(\s|.)*?\}</pre>
    * 
+   * <pre>
+   * default ((.|\s)+?\))\s*\{(\s|.)*?\}
+   * </pre>
+   * 
+   * 使用方法：
+   * 
+   * <pre>
+   * 1. 替换default空实现为接口方法：'$1;'
+  &#64;Override
+  default int countTotalRows() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+  // 替换后
+  &#64;Override
+  int countTotalRows();
+   * </pre>
    */
   public static final String INTERFACE_EMPTY_DEFAULT_METHOD = "default ((.|\\s)+?\\))\\s*\\{(\\s|.)*?\\}";
 }
