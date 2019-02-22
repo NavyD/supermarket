e3e5b99cc994e65b99049cf4cb2f9800d99c71be
navyd <dhjnavyd@gmail.com>
2019-02-22 16:52:39 +0800

    修复product表insert语句不匹配的问题

--- supermarket-common/src/main/resources/db/h2/test-data.sql
+++ supermarket-common/src/main/resources/db/h2/test-data.sql
@@ -5,7 +5,7 @@ insert into role_info(role_name) values('用户'), ('管理员'), ('超级管理
 insert into user_role(user_id, role_id) values(1, 1), (1, 2), (1, 3);
 
 
-insert into product(product_name, production_date, shelf_life, product_unit, weight, weight_unit, product_category_id, supplier_id) values
-('小龙虾', '2018-07-06', 100, 1, 500, 2, 1, 1),
-('酸菜鱼','2018-07-12', 50, 1, 88.88, 2, 1, 1),
-('可口可乐','2018-06-01', 190, 1, 240, 2, 1, 1);
+insert into product(product_name, production_date, shelf_life, product_unit, specification, specification_unit, product_category_id, product_category_name, supplier_id, supplier_name) values
+('小龙虾', '2018-07-06', 100, 1, 500, 2, 1, '食品', 1, '供应商1'),
+('酸菜鱼','2018-07-12', 50, 1, 88.88, 2, 1, '食品', 1, '供应商1'),
+('可口可乐','2018-06-01', 190, 1, 240, 2, 1, '食品', 1, '供应商1');
